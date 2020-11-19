815609877e2cd33987e1fb10745cea24607db34c
Khatri Hitesh <khatri.hitesh@gmail.com>
2020-11-19 14:17:38 +0000

    razzle grommet shop started

---
+++ src/Home.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+
+import "./Home.css";
+import {
+  Box,
+  Button,
+  Collapsible,
+  Heading,
+  Grommet,
+  ResponsiveContext,
+} from "grommet";
+import { Notification } from "grommet-icons";
+
+const theme = {
+  global: {
+    colors: {
+      brand: "#228BE6",
+    },
+    font: {
+      family: "Roboto",
+      size: "18px",
+      height: "20px",
+    },
+  },
+};
+
+const AppBar = (props: any) => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: "medium", right: "small", vertical: "small" }}
+    elevation="medium"
+    style={{ zIndex: "1" }}
+    {...props}
+  />
+);
+
+const Home = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  return (
+    <Grommet theme={theme} full>
+      <ResponsiveContext.Consumer>
+        {(size) => (
+          <Box fill>
+            <AppBar>
+              <Heading level="3" margin="none">
+                My App
+              </Heading>
+              <Button
+                icon={<Notification />}
+                onClick={() => setShowSidebar(!showSidebar)}
+              />
+            </AppBar>
+            <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
+              <Box flex align="center" justify="center">
+                app body
+              </Box>
+              {size !== "small" && (
+                <Collapsible direction="horizontal" open={showSidebar}>
+                  <Box
+                    flex
+                    width="medium"
+                    background="light-2"
+                    elevation="small"
+                    align="center"
+                    justify="center"
+                  >
+                    sidebar
+                  </Box>
+                </Collapsible>
+              )}
+            </Box>
+          </Box>
+        )}
+      </ResponsiveContext.Consumer>
+    </Grommet>
+  );
+};
+
+export default Home;
